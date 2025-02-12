478b5a67d4dbbd69061c93e63af9ea1e168ab213
Sajid <enggsajid11@gmail.com>
2025-02-12 22:19:15 +0500

    my  comment 2

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+export default function Navbar() {
+  return (
+    <div>
+      <header>
+        <nav>
+            <div className="logo">Logo</div>
+            <ul>
+                <li><a href="#">Home</a></li>
+                <li><a href="#">Home</a></li>
+                <li><a href="#">Home</a></li>
+                <li><a href="#">Home</a></li>
+                <li><a href="#">Home</a></li>
+            </ul>
+            <button>Hire Now</button>
+        </nav>
+      </header>
+    </div>
+  )
+}
